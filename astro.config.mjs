24b79ff0677cf8af045f5c6bf45205955ef95725
Vishnu Jaiswal <30700361+zAcKeR-KrAcKeR@users.noreply.github.com>
2025-09-04 15:08:58 +0530

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,5 @@
-// @ts-check
-import { defineConfig } from "astro/config";
-import tailwindcss from "@tailwindcss/vite";
+import { defineConfig } from 'astro/config'
 
-// https://astro.build/config
 export default defineConfig({
-  vite: {
-    plugins: [tailwindcss()],
-  },
-});
+  site: 'https://vishescape.tech',
+})
